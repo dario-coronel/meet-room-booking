bb6eff9065e4bceca51bbbe31e3d54504370f0df
dario-coronel <dario.coronel@gmail.com>
2025-07-17 22:58:23 -0300

    Correct when adding a book

--- src/services/booking_service.py
+++ src/services/booking_service.py
@@ -1,20 +1,23 @@
+from src.models.booking import Booking
 from src.repositories.booking_repository import BookingRepository
 from src.patterns.time_validation_strategy import TimeValidationStrategy
+from datetime import datetime
 
 class BookingService:
     def __init__(self, repository: BookingRepository, validator: TimeValidationStrategy):
         self.repository = repository
         self.validator = validator
 
-    def create_booking(self, room, user, start_time, end_time):
-        new_booking = self.repository.add(room, user, start_time, end_time)
+    def create_booking(self, room, user, start_time: datetime, end_time: datetime):
+        # Create a temporary booking object (not yet added to repo)
+        temp_booking = Booking(-1, room, user, start_time, end_time)
         existing_bookings = self.repository.get_by_room(room.room_id)
 
-        if not self.validator.is_valid(new_booking, existing_bookings):
-            self.repository.delete(new_booking.booking_id)
+        if not self.validator.is_valid(temp_booking, existing_bookings):
             raise ValueError("Time slot overlaps with an existing booking.")
 
-        return new_booking
+        # Now add it for real
+        return self.repository.add(room, user, start_time, end_time)
 
     def get_bookings_by_user(self, user_id: int):
         return self.repository.get_by_user(user_id)
